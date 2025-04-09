55c050f9bc4950e40ca617886d3f2ffcd0bd6d31
parkerburrow <parkerburrow@seattleacademy.org>
2025-04-09 17:08:30 +0000

    set up

---
+++ Projects/project3.py
@@ -0,0 +1,117 @@
+import turtle
+turtle.Screen().bgcolor("black")
+for i in range ( 100 ) :
+    t = turtle.Turtle()
+t.speed(10)
+t.goto (100, 0)
+t.setheading (0)
+t.color("grey")
+
+for i in range (100) :
+    t.forward(50)
+    t.left (91)
+
+t.goto (200, 0)
+t.setheading (0)
+t.color("grey")
+
+for i in range (100) :
+    t.forward(50)
+    t.left (91)
+
+t.goto (300, 0)
+t.setheading (0)
+t.color("grey")
+
+for i in range (100) :
+    t.forward(50)
+    t.left (91)
+
+t.goto (0, 0)
+t.setheading (0)
+t.color("grey")
+
+for i in range (100) :
+    t.forward(50)
+    t.left (91)
+
+t.goto (-100, 0)
+t.setheading (0)
+t.color("grey")
+
+for i in range (100) :
+    t.forward(50)
+    t.left (91)
+
+t.goto (-200, 0)
+t.setheading (0)
+t.color("grey")
+
+for i in range (100) :
+    t.forward(50)
+    t.left (91)
+
+t.goto (-300, 0)
+t.setheading (0)
+t.color("grey")
+
+for i in range (100) :
+    t.forward(50)
+    t.left (91)
+
+t.goto (-300, 100)
+t.setheading (0)
+t.color("grey")
+
+for i in range (100) :
+    t.forward(50)
+    t.left (91)
+
+t.goto (-200, 100)
+t.setheading (0)
+t.color("grey")
+
+for i in range (100) :
+    t.forward(50)
+    t.left (91)
+
+t.goto (-100, 100)
+t.setheading (0)
+t.color("grey")
+
+for i in range (100) :
+    t.forward(50)
+    t.left (91)
+
+    t.goto (0, 100)
+t.setheading (0)
+t.color("grey")
+
+for i in range (100) :
+    t.forward(50)
+    t.left (91)
+
+t.goto (100, 100)
+t.setheading (0)
+t.color("grey")
+
+for i in range (100) :
+    t.forward(50)
+    t.left (91)
+
+t.goto (200, 100)
+t.setheading (0)
+t.color("grey")
+
+for i in range (100) :
+    t.forward(50)
+    t.left (91)
+
+    t.goto (300, 100)
+t.setheading (0)
+t.color("grey")
+
+for i in range (100) :
+    t.forward(50)
+    t.left (91)
+turtle.exitonclick ()
